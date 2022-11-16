986c39a0830265d2baa40f88417ca1340b078304
DWM7721 <77298801+DWM7721@users.noreply.github.com>
2022-11-15 19:39:00 -0500

    added fix for button-two animation on page load

--- Kevin-Powell/Basic,-Intermediate-&-Pro-Hamburger-Icons/main.js
+++ Kevin-Powell/Basic,-Intermediate-&-Pro-Hamburger-Icons/main.js
@@ -4,3 +4,17 @@ buttonThree.addEventListener('click', () => {
     const isOpened = buttonThree.getAttribute('aria-expanded');
     (isOpened === 'false') ? buttonThree.setAttribute('aria-expanded', 'true') : buttonThree.setAttribute('aria-expanded', 'false');
 })
+
+// fix for animation running on page load (button-two)
+const buttonTwo = document.querySelector('.button-two');
+buttonTwo.addEventListener('click', () => {
+    const currentState = buttonTwo.getAttribute('data-state');
+    if (!currentState || currentState === 'closed') {
+        buttonTwo.setAttribute('data-state', 'opened');
+        buttonTwo.setAttribute('aria-expanded', 'true');
+    } else {
+        buttonTwo.setAttribute('data-state', 'closed');
+        buttonTwo.setAttribute('aria-expanded', 'false');
+    }
+    
+})
